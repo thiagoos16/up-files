eabc600c69dd370b7ea34e0b7d8a0c25c05ef931
thiagoos16 <tos@icomp.ufam.edu.br>
2020-03-15 11:17:20 -0400

    add organization folder:  - add multer and morgan  - add routes manager file

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const morgan =  require("morgan");
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded( { extended: true } ));
+app.use(morgan("dev"));
 app.use(cors());
 
 mongoose.connect(
@@ -13,8 +16,6 @@ mongoose.connect(
     }
 );
 
-app.get("/", (req, res) => {
-    res.send("ok") 
-});
+app.use('/api', require('./src/routes'));
 
 app.listen(3001);
